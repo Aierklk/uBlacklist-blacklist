39cd2c367a89352ece165f6efd791a7a6307beb9
Aierk <aierklk@gmail.com>
2022-03-27 18:22:57 +0800

    commit

--- js/issues.js
+++ js/issues.js
@@ -5,6 +5,8 @@ const list = process.argv[2]
 const BASEPATH = `../`
 const cacheFile = `${BASEPATH}cache.txt`
 
-console.info(typeof list)
-
-writeFile(cacheFile, list.join(''), true)
+if (typeof list === 'string') {
+    writeFile(cacheFile, list, true)
+} else {
+    throw 'input is not a string.'
+}
